9b1f37e8caa6363c0a9ab9f0c6d4cf797b230585
Jose Luis Chavez G <iscjlchavez@hotmail.com>
2023-05-20 08:27:05 -0600

    Conociendo Phptipos de datos

--- Intro.php
+++ Intro.php
@@ -1,8 +1,33 @@
 <?php 
 // Abrir script Php  
-
-
-
+/* 
+Tipos de datos mas comunes dentro de la programacion 
+Boolean: Alamacenar valores verdederos o falsos 1 y 0 
+Intenger: Entero
+Float: Numeros con decimales utilizando solo un punto decimal como separador
+String: Cadenas de caracteres
+Tipos de datos compuesto
+array: se utiliza para guardar varios valores
+Object: se almacenan objetos denytro de Php
+Tipos de datos especiales
+resource: Hacen referencia a recursos o archivos conexiones
+null: sin un valor especifico
+*/
+// primero colocar un signo $ -> nombre o identificador a la variable o constante(Mayusculas y Minusculas)
+// $8variable = erroneo
+// $_8variable = correcto
+// $variable8 = correcto
+// $8_variable = incorrecto
+$numero = 55.23;
+$numero1 = 15;
+$Nombre = "Jose luis Chavez";
+$MiVariable;
+$MiVariable = "Hola buenos dias mi Nombre es:";
+// print() imprimir un solo valor o un a sola cadena 
+// echo "" imprimir varios valores o cadenas
+echo "Estoy imprimiendo el valor de la variable numero: ".$numero;
+echo "<br></br>";
+echo $MiVariable, $Nombre;
 
 
 
